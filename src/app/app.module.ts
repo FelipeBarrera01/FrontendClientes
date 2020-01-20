9d6b7cbba1e548f646ce438033d68d6bfd3a7972
FelipeBarrera01 <juan.f.barrera01@gmail.com>
2020-01-20 17:26:20 -0500

    listando clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioComponent,
+    FooterComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule,
